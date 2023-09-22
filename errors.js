99f2b29b9370327864af6eb0cef11933d6af3589
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-09-22 16:58:13 +1000

    errors fixed

--- errors.js
+++ errors.js
@@ -1,6 +1,6 @@
 // Fix the error in the snackSelection code snippet first
 
-const snackSelection = 'Pretzels';
+let snackSelection = 'Pretzels';
 
 if (snackSelection = 'Pretzels') {
   console.log("That'll be $1, please!");
@@ -16,5 +16,9 @@ function first () {
 function second () {
   third();
 }
+
+function third () {
+  return 'new variable';
+};
 
 first();
